11f437ad943801787f67f34cf4cedb573fdb57d5
BoomTech <boomtechny@gmail.com>
2018-02-09 23:47:02 -0500

    Connected Redux to the application to dispatch events and subscribe to state and added. Tested and created action and reducer.

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,25 @@
-/* global it, expect */
+/* global it, expect, jest */
+
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { initialState } from './reducers/';
+import store from './store';
+import { Provider } from 'react-redux';
 Enzyme.configure({ adapter: new Adapter() });
 
 it('App renders without crashing', () => {
-  const component = shallow(<App />);
+  const mockFunction = jest.fn();
+
+  const component = shallow(
+    <Provider store={store}>
+    <App
+      state={initialState}
+      submitTodo={mockFunction}
+    /></Provider>,
+  );
+
   expect(component.exists()).toEqual(true);
 });
