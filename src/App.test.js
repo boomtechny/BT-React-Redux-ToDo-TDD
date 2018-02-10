09883a141007df9bfafa8bfd7079ac4393d983a9
BoomTech <boomtechny@gmail.com>
2018-02-10 00:20:27 -0500

    Edited import for App to use named import by using curly brackets so I would not have to include store and provider in the test

--- src/App.test.js
+++ src/App.test.js
@@ -2,23 +2,22 @@
 
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from './App';
+import {App }from './App';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { initialState } from './reducers/';
-import store from './store';
-import { Provider } from 'react-redux';
+
 Enzyme.configure({ adapter: new Adapter() });
 
 it('App renders without crashing', () => {
   const mockFunction = jest.fn();
 
   const component = shallow(
-    <Provider store={store}>
+  
     <App
       state={initialState}
       submitTodo={mockFunction}
-    /></Provider>,
+    />,
   );
 
   expect(component.exists()).toEqual(true);
